d410e7b6bdb44b177bb72ad3c3d06fc639ffc076
May Cheong <maycheongs@gmail.com>
2025-07-22 12:39:32 -0500

    remove react refresh plugin on craco to prevent import errors

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,7 +1,22 @@
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
 module.exports = {
+  reactRefresh: {
+    // Disable CRA's fast refresh (react-refresh) completely
+    disableFastRefresh: true,
+  },
   style: {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
+    },
+  },
+  webpack: {
+    configure: (webpackConfig) => {
+      // Remove ReactRefreshWebpackPlugin to prevent import errors
+      webpackConfig.plugins = webpackConfig.plugins.filter(
+        (plugin) => !(plugin instanceof ReactRefreshWebpackPlugin)
+      );
+      return webpackConfig;
     },
   },
 };
