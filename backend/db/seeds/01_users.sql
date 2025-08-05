c9356d4f716a4ed8b460048feb5450032c3ddfad
May Cheong <maycheongs@gmail.com>
2025-08-05 14:07:53 -0500

    change default user password to password

--- backend/db/seeds/01_users.sql
+++ backend/db/seeds/01_users.sql
@@ -1,8 +1,8 @@
 INSERT INTO users (first_name, last_name, email, password)
-VALUES ('Alice', 'Wonderland', 'alice@wonderland.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('Queen', 'Hearts', 'queen@hearts.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('Mario', 'Luigi', 'mario@luigi.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('Patrick', 'Star', 'patrick@star.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('Furniture', 'Man', 'furniture@man.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('Journey', 'Demo', 'journeytravelplanner@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
+VALUES ('Alice', 'Wonderland', 'alice@wonderland.com', '$2b$10$d.O8hGy0yp91Zx./SWiVE.PZHBrwNCUKVvZxvMQ.xJtEwKGuAcpFe'),
+('Queen', 'Hearts', 'queen@hearts.com', '$2b$10$d.O8hGy0yp91Zx./SWiVE.PZHBrwNCUKVvZxvMQ.xJtEwKGuAcpFe'),
+('Mario', 'Luigi', 'mario@luigi.com', '$2b$10$d.O8hGy0yp91Zx./SWiVE.PZHBrwNCUKVvZxvMQ.xJtEwKGuAcpFe'),
+('Patrick', 'Star', 'patrick@star.com', '$2b$10$d.O8hGy0yp91Zx./SWiVE.PZHBrwNCUKVvZxvMQ.xJtEwKGuAcpFe'),
+('Furniture', 'Man', 'furniture@man.com', '$2b$10$d.O8hGy0yp91Zx./SWiVE.PZHBrwNCUKVvZxvMQ.xJtEwKGuAcpFe'),
+('Journey', 'Demo', 'journeytravelplanner@gmail.com', '$2b$10$d.O8hGy0yp91Zx./SWiVE.PZHBrwNCUKVvZxvMQ.xJtEwKGuAcpFe')
 ;
