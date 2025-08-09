1bc097f8e7c2e2d42be8eb1edc2b97db4b1fc816
May Cheong <maycheongs@gmail.com>
2025-08-08 21:09:49 -0500

    add cors as cors options and dry it up

--- backend/app.js
+++ backend/app.js
@@ -78,12 +78,15 @@ console.log('CORS origin:', origin); //DEBUG
 //   },
 // }));
 
-app.use(cors({
+const corsOptions = {
   origin: 'https://journey-ncho.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // handle preflight
 app.use(session({
   name: 'connect.sid',
   secret: 'keyboard cat',
