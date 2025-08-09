54ea8c8e7ca60282265fba128f94f843e4918c9b
May Cheong <maycheongs@gmail.com>
2025-08-08 21:16:55 -0500

    add secure as true for cookies to be sent

--- backend/app.js
+++ backend/app.js
@@ -93,8 +93,9 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
   cookie: {
+    secure: true,
     httpOnly: true,
-    path: '/',
+    // path: '/',
     maxAge: 24 * 60 * 60 * 1000,
     sameSite: 'none', // for cross-site cookies
   },
